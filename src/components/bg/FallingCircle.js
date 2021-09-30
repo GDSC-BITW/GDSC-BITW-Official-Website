ee8da757b5d68e15323168a3f5d6ed2c023c6172
Nikhil Tidke <nikhil.st8@gmail.com>
2021-09-30 15:30:29 +0530

    First Debug (Nikhil Tidke) - 30/9/2021

--- src/components/bg/FallingCircle.js
+++ src/components/bg/FallingCircle.js
@@ -4,9 +4,6 @@ import './falling.css'
 const Falling = () => {
 return(
   <>
-    <div className="container">
-
-    </div>
     <ul class="circle">
       <li class="blue"></li>
       <li class="red"></li>
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -12,20 +12,25 @@ const HomeScreen = () => {
       <Background />
       <FallingCircle />
       <Grid container className="hero" direction="column" justifyContent="center" alignItems="center">
-        <Grid lg={8} xl={8} md={8} sm={12} xsm={12} container >
-          <Grid style={{marginBottom:15}}>
+        <Grid lg={8} xl={8} md={8} sm={12} xsm={12} >
+          <div>
             <strong className="heading blue">Google  </strong>
             <strong className="heading red"> Developer   </strong>
             <strong className="heading yellow">  Student  </strong>
             <strong className="heading green"> Club </strong>
-            <p className="desc" >Bajaj Institute of Technology, Wardha</p>
-          </Grid>
-           Google Developer Student Clubs (GDSC) are community groups for college and university students interested in Google developer technologies. ...
-           By joining a GDSC, students grow their knowledge in a peer-to-peer learning environment and build solutions for local businesses and their community.
+            <br/>
+            Bajaj Institute of Technology, Wardha
+          </div>
+          <br/>
+           Google Developer Student Clubs (GDSC) are community groups for college and university students interested in 
+           Google developer technologies. By joining a GDSC, students grow their knowledge in a peer-to-peer learning environment and build 
+           solutions for local businesses and their community.
+           <br/>
           <Button variant="contained" color="primary" className={classes.btn}>
            Learn More
           </Button>
         </Grid>
+        
       </Grid>
 
       <div class="jsx-1703132234 row">
@@ -35,35 +40,44 @@ const HomeScreen = () => {
         <div style={{height:'15px',width:'25%',padding:0,marginLeft:0,marginRight:0,display:'flex' }}class="jsx-1703132234 bg-green"></div>
       </div>
 
-      <Grid container spacing={3} className="sec-section">
-        <Grid sm={12} xsm={12} lg={8} xlg={8} >
-          <strong className="heading blue">OPPORTUNITIES   </strong>
-          <strong className="heading red"> GDSC    </strong>
-          <strong className="heading yellow">  PROVIDES   </strong>
-          <strong className="heading green"> STUDENT </strong>
-          <p className="desc" >Meet other students on campus interested in developer technologies. All are welcome, including those with diverse backgrounds and different majors.</p>
+      <Grid container spacing={5} className="sec-section">
+        <Grid item sm={12} xsm={12} lg={6} xlg={8} >
+          <strong className="heading blue">OPPORTUNITIES</strong>
+          <strong className="heading red"> GDSC</strong>
+          <strong className="heading yellow">  PROVIDES</strong>
+          <strong className="heading green"> STUDENT</strong>
+          <ul className="section-list">
+            <li >
+            <p className="desc" >Meet other students on campus interested in developer technologies. All are welcome, including those with diverse backgrounds and different majors.</p>
+            </li>
+            <li>
+            <p className="desc" >Apply your new learnings and connections to build great solutions for local problems. Advance your skills, career and network.</p>
+            </li>
+          </ul>
+          <Button variant="contained" color="primary" className={classes.btn}>
+           Interested ?
+          </Button>
         </Grid>
-        <Grid sm={12} xsm={12} lg={4} xlg={4} >
-          <img src="/images/hero.png" alt="" className="hero-img" />
+        <Grid item sm={12} xsm={12} lg={4} xlg={4} >
+          <img src="/images/asset1.svg" alt="" className="hero-img" />
         </Grid>
+
       </Grid>
 
-      <Grid container spacing={3} className="third-section">
-
-      </Grid>
-
-      <Grid container spacing={3} className="third-section">
-        <Grid sm={12} xsm={12} lg={4} xlg={4} >
-          <img src="/images/team.png" alt="" className="hero-img" />
+      <Grid container spacing={5} className="third-section">
+        <Grid item sm={12} xsm={12} lg={5} xlg={4} >
+          <img src="/images/asset2.svg" alt="" className="hero-img" />
         </Grid>
-        <Grid sm={12} xsm={12} lg={8} xlg={8} >
-          <strong className="heading blue">OPPORTUNITIES   </strong>
-          <strong className="heading red"> GDSC    </strong>
-          <strong className="heading yellow">  PROVIDES   </strong>
-          <strong className="heading green"> STUDENT </strong>
-          <p className="desc" >Meet other students on campus interested in developer technologies. All are welcome, including those with diverse backgrounds and different majors.</p>
+        <Grid item sm={12} xsm={12} lg={6} xlg={8} >
+          <strong className="heading blue">OPPORTUNITIES</strong>
+          <strong className="heading red"> GDSC</strong>
+          <strong className="heading yellow">  PROVIDES</strong>
+          <strong className="heading green"> STUDENT</strong>
+          <p className="desc section-decription" >Teamwork is the ability to work together toward a common vision. The ability to direct individual accomplishments toward organizational objectives. It is the fuel that allows common people to attain uncommon results.</p>
+          <Button variant="contained" color="primary" className={classes.btn}>
+           Our Team
+          </Button>
         </Grid>
-
       </Grid>
 
     </>
