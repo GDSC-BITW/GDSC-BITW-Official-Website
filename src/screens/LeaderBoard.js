5ddc1c209c72d83df0df473bcc945aaf28f5e974
Nikhil Tidke <nikhil.st8@gmail.com>
2021-10-26 22:21:06 +0530

    Leaderboard data

--- src/screens/LeaderBoard.js
+++ src/screens/LeaderBoard.js
@@ -1,5 +1,6 @@
 import {Grid} from '@material-ui/core/';
-import React from 'react'
+import React from 'react';
+import {useEffect} from 'react';
 import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -8,20 +9,20 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
-
+import data from '../data/data.json';
 
 const columns = [
     { id: 'name', label: 'Name', minWidth: 170 },
     {
       id: 'date',
-      label: 'Completion Date (T-1)',
+      label: 'Completion Status (T-1)',
       minWidth: 170,
       align: 'right',
       format: (value) => value.toLocaleString('en-US'),
     },
     {
         id: 'date2',
-        label: 'Completion Date (T-2)',
+        label: 'Completion Status (T-2)',
         minWidth: 170,
         align: 'right',
         format: (value) => value.toLocaleString('en-US'),
@@ -39,38 +40,49 @@ const columns = [
     return { name, date, link,date2};
   }
   
-  const rows = [
-    createData('India', 'IN', 1324171354, 3287263),
-    createData('China', 'CN', 1403500365, 9596961),
-    createData('Italy', 'IT', 60483973, 301340),
-    createData('United States', 'US', 327167434, 9833520),
-    createData('Canada', 'CA', 37602103, 9984670),
-    createData('Australia', 'AU', 25475400, 7692024),
-    createData('Germany', 'DE', 83019200, 357578),
-    createData('Ireland', 'IE', 4857000, 70273),
-    createData('Mexico', 'MX', 126577691, 1972550),
-    createData('Japan', 'JP', 126317000, 377973),
-    createData('France', 'FR', 67022000, 640679),
-    createData('United Kingdom', 'GB', 67545757, 242495),
-    createData('Russia', 'RU', 146793744, 17098246),
-    createData('Nigeria', 'NG', 200962417, 923768),
-    createData('Brazil', 'BR', 210147125, 8515767),
-  ];
+
+  
 
 
 const LeaderBoard = (props) => { 
 
     const [page, setPage] = React.useState(0);
     const [rowsPerPage, setRowsPerPage] = React.useState(10);
+    const [json, setJson] = React.useState(data);
+    const rows = [];
+
+    json.Sheet1.map((dat)=>
+        rows.push(createData(dat["Name "],dat['Track 1'],dat[" Qwiklabs public profile url"], dat['Track 2']))
+    )
 
     const handleChangePage = (event, newPage) => {
         setPage(newPage);
     };
 
+    console.log(data);
+    
     const handleChangeRowsPerPage = (event) => {
         setRowsPerPage(+event.target.value);
         setPage(0);
     };
+
+    const renderButton = (column,value)=>{
+        if(column.label!='Qwiklabs Link'){
+          return <TableCell key={column.id} align={column.align}>
+                    {column.format && typeof value === 'number'
+                      ? column.format(value)
+                      : value}
+                  </TableCell>
+        } else{
+          return <TableCell key={column.id} align={column.align}>
+                      <a href={column.format && typeof value === 'number'
+                          ? column.format(value)
+                          : value}>
+                        Qwiklabs
+                      </a>
+                  </TableCell>
+        }
+    }
 
     return(
      <div>
@@ -108,11 +120,7 @@ const LeaderBoard = (props) => {
                                 {columns.map((column) => {
                                 const value = row[column.id];
                                 return (
-                                    <TableCell key={column.id} align={column.align}>
-                                    {column.format && typeof value === 'number'
-                                        ? column.format(value)
-                                        : value}
-                                    </TableCell>
+                                    renderButton(column,value)
                                 );
                                 })}
                             </TableRow>
