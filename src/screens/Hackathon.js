a83db23c96745ca6d5ae009d352d0adccb868246
Bhavesh Anandpara <bhaveshAP09112002@gmail.com>
2022-03-04 22:36:42 +0530

    hackathon page mobile responsive

--- src/screens/Hackathon.js
+++ src/screens/Hackathon.js
@@ -3,7 +3,6 @@ import { Grid, Typography, Button, Item } from '@material-ui/core/';
 import useStyles from '../commonStyles'
 import Background from '../components/bg/Background'
 import { NavLink, Link } from "react-router-dom";
-import FallingCircle from '../components/bg/FallingCircle'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import { fontWeight, width } from '@mui/system';
@@ -19,11 +18,12 @@ const HackathonScreen = () => {
             <Grid container className="hero" justifyContent="center" alignItems="center" style={{ paddingTop: "10vh" }}>
                 <Grid lg={8} xl={8} md={8} sm={12} xsm={12} justifyContent="center" className='hack-hero' alignItems="center">
                     <Grid >
-                        <strong className="heading blue">Google  </strong>
-                        <strong className="heading red"> Developer   </strong>
-                        <strong className="heading yellow">  Student  </strong>
-                        <strong className="heading green"> Club </strong>
-                        <br />
+                        <p className='center'>
+                            <strong className="heading blue">Google  </strong>
+                            <strong className="heading red"> Developer   </strong>
+                            <strong className="heading yellow">  Student  </strong>
+                            <strong className="heading green"> Club </strong>
+                        </p>
                         <p className="center"><strong >Bajaj Institute of Technology, Wardha</strong></p>
                     </Grid>
                     <Grid>
@@ -42,13 +42,18 @@ const HackathonScreen = () => {
 
             </Grid>
 
-            <Grid container spacing={5} className="third-section" justifyContent="center" alignItems="center" style={{ marginLeft: "50px" }}>
+            <Grid container className="aboutHackathon" justifyContent="center" alignItems="center" >
                 <Grid item sm={12} xsm={12} lg={4} xlg={4} justifyContent="center" alignItems="center" >
                     <img src="../images/asset-3.jpg" alt="aa" className="hero-img" />
                 </Grid>
-                <Grid item sm={12} xsm={12} lg={6} xlg={8} justifyContent="center" alignItems="center" style={{ marginLeft: "20px" }}>
+
+                <Grid item item sm={12} xsm={12} lg={6} xlg={8} justifyContent="center" alignItems="center" style={{ marginLeft: "20px" }}>
                     <strong className="heading blue">ABOUT</strong>
-                    <p style={{ marginTop: '-25px' }} ><strong className="heading "> <span className='red'>SMART</span> <span className='yellow'>-O-</span> <span className='green'>THON</span></strong></p>
+                    <p style={{ marginTop: '-10px' }} >
+                        <strong className="heading red">SMART</strong>
+                        <strong className="heading yellow">-O-</strong>
+                        <strong className="heading green">THON</strong>
+                    </p>
 
                     <p className="desc section-decription" >Participants will be given a list of domains from which
                         they must choose one and come up with a solution to
@@ -88,45 +93,45 @@ const HackathonScreen = () => {
 
             <Grid container spacing={5} className="section-center">
                 <Grid lg={12} style={{ margin: '50px 0px' }} >
-                    <p className='center'><strong className='heading blue' style={{ marginLeft: '-80px' }} >DOMAINS</strong></p>
+                    <p className='center'><strong className='heading blue' >DOMAINS</strong></p>
                 </Grid>
-                <Grid item className='desc' lg={3}>Disaster Managment</Grid>
-                <Grid item className='desc' lg={3}>MedTech / BioTech / HealthTech</Grid>
-                <Grid item className='desc' lg={3}>Blockchain & Cybersecurity</Grid>
-                <Grid item className='desc' lg={3}>Smart Vehicles</Grid>
-                <Grid item className='desc' lg={3}>Smart Automation</Grid>
-                <Grid item className='desc' lg={3}>Smart Education</Grid>
-                <Grid item className='desc' lg={3}>Transportation & Logistics</Grid>
-                <Grid item className='desc' lg={3}>Heritage & Culture</Grid>
-                <Grid item className='desc' lg={3}>Agriculture, FoodTech & Rural
+                <Grid item className='desc domains' lg={3}>Disaster Managment</Grid>
+                <Grid item className='desc domains' lg={3}>MedTech / BioTech / HealthTech</Grid>
+                <Grid item className='desc domains' lg={3}>Blockchain & Cybersecurity</Grid>
+                <Grid item className='desc domains' lg={3}>Smart Vehicles</Grid>
+                <Grid item className='desc domains' lg={3}>Smart Automation</Grid>
+                <Grid item className='desc domains' lg={3}>Smart Education</Grid>
+                <Grid item className='desc domains' lg={3}>Transportation & Logistics</Grid>
+                <Grid item className='desc domains' lg={3}>Heritage & Culture</Grid>
+                <Grid item className='desc domains' lg={3}>Agriculture, FoodTech & Rural
                     Development</Grid>
 
 
             </Grid>
 
-            <Grid container spacing={5} className="section-center" style={{transform:'translate(-47%,0%)'}} >
-                <Grid lg={12} style={{ margin: '50px 0px'}} >
-                    <p className='center'>  
+            <Grid container spacing={5} className="section-center" >
+                <Grid lg={12} style={{ margin: '50px 0px' }} >
+                    <p className='center'>
                         <strong className="heading blue">GLIMPSES  </strong>
                         <strong className="heading red"> OF   </strong>
                         <strong className="heading yellow">  LAST  </strong>
                         <strong className="heading green"> YEAR </strong></p>
                 </Grid>
-                <Grid item className='desc' lg={4}> <div className='glimpse-img' style={{ backgroundImage:'url(../images/Hackathon/3.JPG)' }}></div> </Grid>
-                <Grid item className='desc' lg={4}> <div className='glimpse-img' style={{ backgroundImage:'url(../images/Hackathon/4.JPG)' }}></div> </Grid>
-                <Grid item className='desc' lg={4}> <div className='glimpse-img' style={{ backgroundImage:'url(../images/Hackathon/5.JPG)' }}></div> </Grid>
-                <Grid item className='desc' lg={4}> <div className='glimpse-img' style={{ backgroundImage:'url(../images/Hackathon/6.JPG)' }}></div> </Grid>
-                <Grid item className='desc' lg={4}> <div className='glimpse-img' style={{ backgroundImage:'url(../images/Hackathon/7.JPG)' }}></div> </Grid>
-                <Grid item className='desc' lg={4}> <div className='glimpse-img' style={{ backgroundImage:'url(../images/Hackathon/8.JPG)' }}></div> </Grid>
-                <Grid item className='desc' lg={4}> <div className='glimpse-img' style={{ backgroundImage:'url(../images/Hackathon/9.JPG)' }}></div> </Grid>
-               
+                <Grid item className='desc' lg={4}> <div className='glimpse-img' style={{ backgroundImage: 'url(../images/Hackathon/3.JPG)' }}></div> </Grid>
+                <Grid item className='desc' lg={4}> <div className='glimpse-img' style={{ backgroundImage: 'url(../images/Hackathon/4.JPG)' }}></div> </Grid>
+                <Grid item className='desc' lg={4}> <div className='glimpse-img' style={{ backgroundImage: 'url(../images/Hackathon/5.JPG)' }}></div> </Grid>
+                <Grid item className='desc' lg={4}> <div className='glimpse-img' style={{ backgroundImage: 'url(../images/Hackathon/6.JPG)' }}></div> </Grid>
+                <Grid item className='desc' lg={4}> <div className='glimpse-img' style={{ backgroundImage: 'url(../images/Hackathon/7.JPG)' }}></div> </Grid>
+                <Grid item className='desc' lg={4}> <div className='glimpse-img' style={{ backgroundImage: 'url(../images/Hackathon/8.JPG)' }}></div> </Grid>
+                <Grid item className='desc' lg={4}> <div className='glimpse-img' style={{ backgroundImage: 'url(../images/Hackathon/9.JPG)' }}></div> </Grid>
+
 
 
             </Grid>
 
-            <Grid container spacing={5} className="section-center" style={{transform:'translate(-49%,0%)' ,  margin: '50px 0px'}} >
-                <Grid lg={12} style={{ margin: '50px 0px'}} >
-                    <p className='center'>  
+            <Grid container spacing={5} className="section-center" style={{ marginLeft: '-30px' }} >
+                <Grid lg={12} style={{ margin: '50px 0px' }} >
+                    <p className='center'>
                         <strong className="heading blue"> DEPARTMENT COORDINATORS </strong></p>
                 </Grid>
                 <Grid item className='desc' lg={6}>
@@ -138,15 +143,15 @@ const HackathonScreen = () => {
                     <p className='center haeding-2'>Mr. Sandesh Jain</p>
                 </Grid>
                 <Grid item className='desc' lg={6}>
-                    <p className="heading-2 center">Electrical Engineering 
-</p>
+                    <p className="heading-2 center">Electrical Engineering
+                    </p>
                     <p className='center haeding-2'>Dr. Jayesh Ruikar</p>
                 </Grid>
                 <Grid item className='desc' lg={6}>
                     <p className="heading-2 center">Mechanical Engineering</p>
                     <p className='center haeding-2'>Dr. Pawan Chandak</p>
                 </Grid>
-               
+
 
             </Grid>
 
